299fae29d54d732ee15eea14306bb8a0e8d240db
leninne44 <leninne.pirovano@scheffer.agr.br>
2022-06-10 10:47:17 -0400

    isso

--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from core.serializers import ClienteSerializer
 from rest_framwork import viewsets
 from .models import Cliente
+from .serializers import ClienteSerializer
 
 class ClienteViewSet(viewsets.ModelViewSet):
     queryset = Cliente.object.all()
